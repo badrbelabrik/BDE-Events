45102b2568d60540c0c629583574e2fdef7977ff
badrbelabrik <badr.belabrik@gmail.com>
2026-07-23 17:13:48 +0100

    display tickets

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,8 +13,11 @@ class StudentController extends Controller
 {
     public function index(){
         $events = Event::with('user')->latest()->get();
+        $tickets = Reservation::with(['event', 'ticket'])
+            ->where('user_id', Auth::id())
+            ->get();
 
-        return view('student', compact('events'));
+        return view('student', compact('events','tickets'));
     }
     public function subscribe(Event $event){
 
